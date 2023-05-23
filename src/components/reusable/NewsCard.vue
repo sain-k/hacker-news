658048d1c51b25126e9a4340b0c85190d949354d
sabin-k <sabinkhanal.dev@gmail.com>
2023-05-23 11:49:08 +0545

    task 4 done

--- src/components/reusable/NewsCard.vue
+++ src/components/reusable/NewsCard.vue
@@ -21,7 +21,7 @@ function openLink(url) {
       {{ hit.title }}
     </p>
 
-    <div class="flex items-center mt-[2px]">
+    <div class="flex items-center mt-1">
       <svg
         xmlns="http://www.w3.org/2000/svg"
         fill="none"
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -14,18 +14,36 @@ const currentPage = ref(1)
 const searchTerm = ref('')
 const tags = ref('front_page')
 const cancelToken = ref(null)
+const sortBy = ref('popular')
+const showSortMenu = ref(false)
 
 onMounted(() => {
   fetchData()
 })
 
-watch([currentPage, searchTerm], () => {
+watch([currentPage, searchTerm, sortBy], () => {
   fetchData()
 })
 
 watch([searchTerm], () => {
   currentPage.value = 1
 })
+
+function selectSortingOptions(value) {
+  currentPage.value = 1
+  searchTerm.value = ''
+  sortBy.value = value
+  showSortMenu.value = false
+}
+
+function clearSearch() {
+  searchTerm.value = ''
+}
+
+function refresh() {
+  currentPage.value = 1
+  fetchData()
+}
 
 function highlightTextContaining(pattern) {
   const regex = new RegExp(pattern, 'gi')
@@ -60,19 +78,36 @@ async function fetchData() {
   error.value = null
   cancelToken.value = axios.CancelToken.source()
 
+  let url = `https://hn.algolia.com/api/v1/search`
+
   if (searchTerm.value.length > 0) {
     tags.value = 'story'
   } else {
     tags.value = 'front_page'
   }
 
+  let params = {
+    tags: tags.value
+  }
+
+  if (sortBy.value == 'latest') {
+    url = `https://hn.algolia.com/api/v1/search_by_date`
+    params = {
+      tags: 'story'
+    }
+  }
+
+  if (currentPage.value > 1) {
+    params.page = currentPage.value
+  }
+
+  if (searchTerm.value.length > 0) {
+    params.query = searchTerm.value
+  }
+
   try {
-    const response = await axios.get('https://hn.algolia.com/api/v1/search', {
-      params: {
-        tags: tags.value,
-        page: currentPage.value,
-        query: searchTerm.value
-      },
+    const response = await axios.get(url, {
+      params,
       cancelToken: cancelToken.value.token
     })
     data.value = response.data
@@ -97,11 +132,60 @@ async function fetchData() {
     <navbar />
     <!-- content -->
     <div class="w-full flex flex-col items-center">
-      <div class="w-4/6 py-6 min-h-[630px] flex flex-col">
-        <div class="flex items-end justify-end">
-          <div class="flex w-[50%] items-center relative">
+      <div class="md:w-4/6 px-4 py-6 md:min-h-[660px] min-h-[715px] w-full flex flex-col">
+        <div class="flex justify-between items-center">
+          <div class="w-[40%]">
+            <div class="relative inline-block text-left">
+              <div>
+                <button
+                  class="inline-flex justify-center w-full rounded-md border-[1.5px] border-primary shadow-sm px-4 py-2 bg-white text-sm font-light font-opensans text-gray focus:outline-none focus:ring-[1px] focus:ring-primary"
+                  @click="showSortMenu = !showSortMenu"
+                >
+                  Sort By
+                  <svg
+                    class="-mr-1 ml-2 h-5 w-5 fill-gray"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                      clip-rule="evenodd"
+                    />
+                  </svg>
+                </button>
+              </div>
+
+              <div
+                v-if="showSortMenu"
+                class="origin-top-right absolute left-0 mt-2 w-[102px] p-1 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none"
+              >
+                <button
+                  @click="selectSortingOptions('popular')"
+                  class="text-gray-700 block w-full py-2 text-sm hover:bg-gray hover:text-white rounded-md mb-[4px]"
+                  :class="{
+                    'bg-primaryLight text-white pointer-events-none': sortBy == 'popular'
+                  }"
+                >
+                  Popular
+                </button>
+                <button
+                  @click="selectSortingOptions('latest')"
+                  class="text-gray-700 block w-full py-2 text-sm hover:bg-gray hover:text-white rounded-md mt-[4px]"
+                  :class="{
+                    'bg-primaryLight text-white pointer-events-none': sortBy == 'latest'
+                  }"
+                >
+                  Latest
+                </button>
+              </div>
+            </div>
+          </div>
+
+          <div class="flex w-[60%] items-center relative">
             <input
-              class="h-10 w-full pl-9 pr-2 rounded-md border-[1.5px] border-primary outline-none hover:ring-[1px] ring-primary font-opensans font-light text-black text-base tracking-wide placeholder:text-gray placeholder:text-base"
+              class="h-10 w-full pl-9 pr-2 rounded-md border-[1.5px] border-primary outline-none focus:ring-[1px] ring-primary font-opensans font-light text-black text-base tracking-wide placeholder:text-gray placeholder:text-base"
               v-model="searchTerm"
               placeholder="Find stories"
             />
@@ -124,11 +208,32 @@ async function fetchData() {
         </div>
 
         <div class="text-left mt-6">
-          <h3 class="text-xl font-semibold font-opensans text-primaryLight">Latest News</h3>
+          <h3 class="text-xl font-semibold font-opensans text-primaryLight capitalize">
+            {{ sortBy }} News
+          </h3>
         </div>
 
-        <div v-if="!loading && data?.hits.length == 0">
+        <div
+          v-if="!loading && data?.hits.length == 0"
+          class="flex flex-col items-center justify-center"
+        >
           <p class="text-center text-base font-light font-opensans text-gray">No News Found</p>
+
+          <button
+            v-if="searchTerm.length > 0"
+            class="flex bg-primaryLight rounded px-2 py-1 outline-none mt-3 text-xs font-normal font-opensans text-white"
+            @click="clearSearch"
+          >
+            Clear Search
+          </button>
+
+          <button
+            v-if="data.hits.length == 0"
+            class="flex bg-primaryLight rounded px-2 py-1 outline-none mt-3 text-xs font-normal font-opensans text-white"
+            @click="refresh"
+          >
+            Reload
+          </button>
         </div>
 
         <div v-if="loading">
